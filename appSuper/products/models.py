1543ef765771af86c9527616fac90d7f4df0306c
LucasLeone <lucasleonetatto@gmail.com>
2020-09-23 04:49:04 -0300

    Solucionado error del admin de products agregando la variable blank a price_offer

--- appSuper/products/models.py
+++ appSuper/products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=255)
     price = models.FloatField()
-    price_offer = models.FloatField(null=True)
+    price_offer = models.FloatField(null=True, blank=True)
     photo = models.ImageField(upload_to='products/photos')
 
     created = models.DateTimeField(auto_now_add=True)
